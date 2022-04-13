ebcae940ee32c3dae4e0bb872f9cefe3ce212fdf
Aldrin Joe Mathew <aldrin@qburst.com>
2022-04-13 22:33:35 +0530

    Layout issue when title span is single line for Popular card - Fixed

--- app/src/main/java/com/nyt/nytnews/ui/screens/NewsFeed.kt
+++ app/src/main/java/com/nyt/nytnews/ui/screens/NewsFeed.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -393,7 +394,11 @@ fun PopularNewsCard(
                 top = BaseSeparation,
                 bottom = 0.dp
             )) {
+                val titleLineHeight = MaterialTheme.typography.h6.fontSize * 4/3
                 Text(
+                    modifier = Modifier.height(with(LocalDensity.current) {
+                        (titleLineHeight * 2).toDp()
+                    }),
                     text = article.headline,
                     style = MaterialTheme.typography.h6,
                     maxLines = 2,
